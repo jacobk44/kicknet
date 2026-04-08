058873d19ef4d87553431c26095264882b1cd06f
jacobk44 <jacobkwartengamoah@gmail.com>
2026-04-08 13:44:08 -0700

    fix my code

---
+++ app/contacts/page.tsx
@@ -0,0 +1,13 @@
+// app/contacts/page.tsx
+"use client"; // only if you use client-side hooks
+
+import React from "react";
+
+export default function ContactsPage() {
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">Contacts</h1>
+      <p>Welcome to the contacts page.</p>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/ui/home/navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Kick Net Store ",
@@ -26,7 +16,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={` h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         {/* Navbar at the top */}
